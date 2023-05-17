4918bc8a747e3bea1e2d924bf205fd0426673deb
GabListeM <gabrielncblender@gmail.com>
2023-05-17 08:29:21 -0300

    tem alguns erros que devem ser revisados

--- WebApp_Manha/Controllers/ProdutosController.cs
+++ WebApp_Manha/Controllers/ProdutosController.cs
@@ -4,9 +4,30 @@ namespace WebApp_Manha.Controllers
 {
     public class ProdutosController : Controller
     {
+        private Contexto db;
+
+        public ProdutosController(Contexto contexto)
+        {
+            db = contexto;
+        }
+
         public IActionResult Lista()
         {
             return View();
         }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    
+    }
+    [HttpPost]
+       public IActionResult SalvarDados(Produtos dados)
+
+    {
+        db.Produtos.Add(dados);
+        db.SaveChanges();
+        return RedirectToAction("Lista")
     }
 }
